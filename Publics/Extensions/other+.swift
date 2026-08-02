f9706d760acc4c1922052f9ab6a2a6037eb738b4
Neo <admin@wzs.app>
2026-08-02 22:23:53 +0800

    add translate

--- Publics/Extensions/other+.swift
+++ Publics/Extensions/other+.swift
@@ -1,18 +1,49 @@
 //
-//  Color+.swift
-//  NoLet
+//  SWIFT: 6.0 - MACOS: 15.7 
+//  NoLet - Dictionary+.swift
 //
 //  Author:        Copyright (c) 2024 QingHe. All rights reserved.
 //  Document:      https://wiki.wzs.app
 //  E-mail:        to@wzs.app
-//
+
+//  Description:
+
 //  History:
-//    Created by Neo on 2025/6/22.
-//
+//    Created by Neo on 2026/8/2 17:09.
 
 import Foundation
-import SwiftUI
 import UIKit
+
+extension Dictionary {
+    static func + (lhs: inout Dictionary, rhs: Dictionary) {
+        lhs.merge(rhs) { _, new in new }
+    }
+
+    static func += (lhs: inout Dictionary, rhs: Dictionary) {
+        lhs.merge(rhs) { _, new in new }
+    }
+}
+
+extension Dictionary where Key == String, Value == String {
+    func text() -> String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+
+        guard let data = try? encoder.encode(self) else {
+            return nil
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+}
+
+
+// MARK: -  keyPath+.swift
+
+func == <T, Value: Equatable>(keyPath: KeyPath<T, Value>, value: Value) -> (T) -> Bool {
+    { $0[keyPath: keyPath] == value }
+}
+
 
 nonisolated extension UIColor {
     convenience init?(hexString: String) {
